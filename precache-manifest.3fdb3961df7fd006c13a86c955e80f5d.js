586aa670753dd2ae22a3bc7e01f6db6be382fdae
Rishav Rungta <rishav394@gmail.com>
2020-08-12 18:56:51 +0530

    Updates

---
+++ precache-manifest.3fdb3961df7fd006c13a86c955e80f5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a792cac5fecb27a47dbb84cabb441773",
+    "url": "/Razorpay-frontend/index.html"
+  },
+  {
+    "revision": "77b4b191974a090642ee",
+    "url": "/Razorpay-frontend/static/css/main.8aeed761.chunk.css"
+  },
+  {
+    "revision": "5d68997f1887f3b5739e",
+    "url": "/Razorpay-frontend/static/js/2.c21b0c08.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/Razorpay-frontend/static/js/2.c21b0c08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b4b191974a090642ee",
+    "url": "/Razorpay-frontend/static/js/main.6e1c473d.chunk.js"
+  },
+  {
+    "revision": "4a33a5da226ec83ec5e9",
+    "url": "/Razorpay-frontend/static/js/runtime-main.7ff5d26f.js"
+  }
+]);
